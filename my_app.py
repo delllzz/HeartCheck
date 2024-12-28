83b70bdce9f0de994b04dd69ab6f3136df6cae3c
delllzz <kusumab375@gmail.com>
2024-12-28 16:49:16 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,31 +1,48 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QApplication, Qwidgets,
-    QHBoxLayout, QVBoxLayout,
-    QGroupBox, QRadioButoon,
-    QPushButton, QLabel, QListWidget, QLineEdit)
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
 
-from instr import*
-from second_win import*
-
+from instr import *
+from second_win import *
+     
 class MainWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.connects()
-        self.set_appear()
-        self.show()
+   def __init__(self):
+       ''' the window which the greeting is located in '''
+       super().__init__()
+       # creating and configuring graphic elements:
+       self.initUI()
+       #establishes connections between elements
+       self.connects()
+       # sets what the window will look like (label, size, location)
+       self.set_appear()
+       # start:
+       self.show()
 
-    def initUI(self):
-        self.btn_next = QPushButton(txt_next, self)
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
-        self.selfLayput(self.Layout_Line)
+   def initUI(self):
+       self.btn_next = QPushButton(txt_next, self)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout_line)
+  
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
 
-    def next_click(self):
-        self.tw = TestWin()
-        self.hide()
+   def connects(self):
+       self.btn_next.clicked.connect(self.next_click)
+
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
